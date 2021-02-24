db0436323800412906d2da7494654689f54108f5
Tobias <ruck.tobias@gmail.com>
2021-02-24 12:12:07 -0500

    Enforce coinbase rule from Genesis. (#12)

--- test/functional/abc_feature_minerfund.py
+++ test/functional/abc_feature_minerfund.py
@@ -16,8 +16,6 @@ from test_framework.util import (
 
 from decimal import Decimal
 
-AXION_ACTIVATION_TIME = 2000000600
-
 MINER_FUND_RATIO = 8
 
 MINER_FUND_ADDR = 'bchreg:pqnqv9lt7e5vjyp0w88zf2af0l92l8rxdgd35g0pkl'
@@ -29,40 +27,20 @@ class MinerFundTest(BitcoinTestFramework):
         self.num_nodes = 1
         self.extra_args = [[
             '-enableminerfund',
-            '-axionactivationtime={}'.format(AXION_ACTIVATION_TIME),
         ]]
 
     def run_test(self):
         node = self.nodes[0]
         address = node.get_deterministic_priv_key().address
 
-        self.log.info('Create some history')
-        for _ in range(0, 50):
-            node.generatetoaddress(1, address)
+        # Mine exactly one block after genesis
+        node.generatetoaddress(1, address)
 
         node = self.nodes[0]
         address = node.get_deterministic_priv_key().address
 
-        # Move MTP forward to axion activation
-        node.setmocktime(AXION_ACTIVATION_TIME)
-        node.generatetoaddress(6, address)
-        assert_equal(
-            node.getblockchaininfo()['mediantime'],
-            AXION_ACTIVATION_TIME)
-
-        # Let's remember the hash of this block for later use.
-        fork_block_hash = int(node.getbestblockhash(), 16)
-
         def get_best_coinbase():
             return node.getblock(node.getbestblockhash(), 2)['tx'][0]
-
-        # No money goes to the fund.
-        coinbase = get_best_coinbase()
-        assert_equal(len(coinbase['vout']), 1)
-        block_reward = coinbase['vout'][0]['value']
-
-        # First block with the new rules.
-        node.generatetoaddress(1, address)
 
         # Now we send part of the coinbase to the fund.
         coinbase = get_best_coinbase()
@@ -75,18 +53,20 @@ class MinerFundTest(BitcoinTestFramework):
         for o in coinbase['vout']:
             total += o['value']
 
+        block_reward = 50
         assert_equal(total, block_reward)
         assert_greater_than_or_equal(
             coinbase['vout'][1]['value'],
             (MINER_FUND_RATIO * total) / 100)
 
-        # Invalidate top block, submit a custom block that do not send anything
-        # to the fund and check it is rejected.
-        node.invalidateblock(node.getbestblockhash())
+        # Submit a custom block that does not send anything
+        # to the fund and check if it is rejected.
+        best_block_hash = node.getbestblockhash()
+        block_time = node.getblockheader(best_block_hash)['time']
 
         block_height = node.getblockcount() + 1
         block = create_block(
-            fork_block_hash, create_coinbase(block_height), AXION_ACTIVATION_TIME + 1, version=4)
+            int(best_block_hash, 16), create_coinbase(block_height), block_time + 1, version=4)
         block.solve()
 
         assert_equal(node.submitblock(ToHex(block)), 'bad-cb-minerfund')
