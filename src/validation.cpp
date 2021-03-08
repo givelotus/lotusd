38d48cc53dc06257e6532a502fe6037e87c4fd0b
Tobias <ruck.tobias@gmail.com>
2021-03-08 11:20:40 -0500

    Remove activation heights, timestamps and predicates. (#56)

--- src/validation.cpp
+++ src/validation.cpp
@@ -278,7 +278,7 @@ bool CheckSequenceLocks(const CTxMemPool &pool, const CTransaction &tx,
 static bool IsReplayProtectionEnabled(const Consensus::Params &params,
                                       int64_t nMedianTimePast) {
     return nMedianTimePast >= gArgs.GetArg("-replayprotectionactivationtime",
-                                           params.tachyonActivationTime);
+        0xffffffff); // TODO: define upgrade schedule
 }
 
 static bool IsReplayProtectionEnabled(const Consensus::Params &params,
