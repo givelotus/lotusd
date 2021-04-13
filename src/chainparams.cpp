063142e5c3216341c5b900f1e13cf5931fa1e4c3
Tobias <ruck.tobias@gmail.com>
2021-04-13 09:36:33 -0500

    Update testnet genesis block (#141)
    - New coinbase string
    - Genesis output script is OP_RETURN; is unspendable anyway
    - Set genesis time to 1618199040, which is 12th April 2021, 03:44:00 UTC

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,6 +70,14 @@ CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits,
                               nBits, nVersion, genesisReward);
 }
 
+CBlock CreateLogosGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits,
+                               int32_t nVersion, const Amount genesisReward) {
+    const char *pszTimestamp = "John 1:1 In the beginning was the Logos";
+    const CScript genesisOutputScript = CScript() << OP_RETURN;
+    return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce,
+                              nBits, nVersion, genesisReward);
+}
+
 /**
  * Main network
  */
@@ -262,15 +270,15 @@ public:
         m_assumed_chain_state_size =
             ChainParamsConstants::TESTNET_ASSUMED_CHAINSTATE_SIZE;
 
-        genesis =
-            CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateLogosGenesisBlock(1618199040, 2402150191, 0x1d00ffff, 1,
+                                          260 * LOTUS);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock ==
-               uint256S("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526"
-                        "f8d77f4943"));
+               uint256S("000000007fcfdfe97c71d4eb5b83c7c2855557daf5d6fd104f4670"
+                        "c7856ba74e"));
         assert(genesis.hashMerkleRoot ==
-               uint256S("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b"
-                        "7afdeda33b"));
+               uint256S("2ea66f6f2a723a125629edcf156cab217f9bbd6441558f77c85e90"
+                        "0ed6554793"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
